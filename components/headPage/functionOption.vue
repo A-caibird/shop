485370e32aa5e2dfcb59142b8b7acf713b824ce7
Acaibird <lian.wk04201@foxmail.com>
2023-07-25 10:19:21 +0800

    [feat]: 新增首页商品展示item组件,功能导航区组件(uniapp,宫格组件)

---
+++ components/headPage/functionOption.vue
@@ -0,0 +1,108 @@
+<template>
+	<view>
+		<view class="shop-card">
+			<view class="image-box">
+				<image :src="imagePath">
+				</image>
+			</view>
+			<view class="title">
+				{{title}}
+			</view>
+			<view class="sec-title">
+				{{secTitle.sorce}}分｜月售{{secTitle.sales}}｜{{secTitle.distance}}m
+			</view>
+			<view class="tag-location">
+				<view class="tag">
+					{{tag}}
+				</view>
+				<view class="location">
+					{{location}}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: ['imagePath', 'title', 'secTitle', 'tag', 'location'],
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.shop-card {
+		width: 342rpx;
+		height: 558rpx;
+		background: #FFFFFF;
+		border-radius: 16rpx;
+
+
+		.image-box {
+			width: 342rpx;
+			height: 342rpx;
+
+			image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+
+		.title {
+			width: 320rpx;
+			height: 80rpx;
+			font-size: 28rpx;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #333333;
+			line-height: 40rpx;
+			margin-top: 20rpx;
+			margin-left: 20rpx;
+			margin-right: 20rpx;
+		}
+
+		.sec-title {
+			width: 258rpx;
+			height: 34rpx;
+			font-size: 24rpx;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #999999;
+			line-height: 34rpx;
+			margin: 20rpx 0rpx 0rpx 20rpx;
+		}
+
+		.tag-location {
+			display: flex;
+			flex-direction: row;
+			margin-left: 20rpx;
+			margin-right: 20rpx;
+			margin-top: 20rpx;
+
+			.tag {
+				width: 44rpx;
+				height: 32rpx;
+				font-size: 22rpx;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #FF7A00;
+				line-height: 32rpx;
+			}
+
+			.location {
+				width: 88rpx;
+				height: 32rpx;
+				font-size: 22rpx;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #FF7A00;
+				line-height: 32rpx;
+			}
+		}
+
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -15,7 +15,7 @@
 					</image>
 				</view>
 				<view class="input-box">
-					<input  placeholder="搜索店铺" placeholder-class="placeholder">
+					<input placeholder="搜索店铺" placeholder-class="placeholder">
 				</view>
 				<view class="btn">
 					<button>
@@ -24,17 +24,86 @@
 				</view>
 			</view>
 		</view>
+		<view class="shop-card-area" >
+			<function-option v-for="(item,index) in foodList" :key="index" :imagePath="item.imagePath" :title="item.title" :secTitle="item.secTitle" :tag="item.tag" :location="item.location"></function-option>
+		</view>
+		<view class="footer">
+			
+		</view>
 	</view>
 </template>
 <script>
+	import functionOption from "@/components/headPage/functionOption.vue"
 	export default {
+		components: {
+			functionOption,
+		},
 		data() {
 			return {
-
+				foodList: [{
+					imagePath: '/static/headPage/footPhoto1.png',
+					title: '大叔小子烤鱼（天一广场店',
+					secTitle: {
+						sorce: '4.6',
+						sales: '623',
+						distance: '120'
+					},
+					tag: '烤鱼',
+					location: '天一广场'
+				},
+				{
+					imagePath: '/static/headPage/footPhoto1.png',
+					title: '大叔小子烤鱼（天一广场店',
+					secTitle: {
+						sorce: '4.6',
+						sales: '623',
+						distance: '120'
+					},
+					tag: '烤鱼',
+					location: '天一广场'
+				},
+				{
+					imagePath: '/static/headPage/footPhoto1.png',
+					title: '大叔小子烤鱼（天一广场店',
+					secTitle: {
+						sorce: '4.6',
+						sales: '623',
+						distance: '120'
+					},
+					tag: '烤鱼',
+					location: '天一广场'
+				},
+				{
+					imagePath: '/static/headPage/footPhoto1.png',
+					title: '大叔小子烤鱼（天一广场店',
+					secTitle: {
+						sorce: '4.6',
+						sales: '623',
+						distance: '120'
+					},
+					tag: '烤鱼',
+					location: '天一广场'
+				},
+				{
+					imagePath: '/static/headPage/footPhoto1.png',
+					title: '大叔小子烤鱼（天一广场店',
+					secTitle: {
+						sorce: '4.6',
+						sales: '623',
+						distance: '120'
+					},
+					tag: '烤鱼',
+					location: '天一广场'
+				}],
 			}
 		},
 		methods: {
-
+			left() {
+				console.log('left');
+			},
+			right() {
+				console.log('right');
+			}
 		},
 		mounted() {
 
@@ -63,6 +132,7 @@
 				flex-direction: row;
 				place-items: center;
 				position: relative;
+
 				.text {
 					font-size: 28rpx;
 					font-family: PingFangSC-Medium, PingFang SC;
@@ -79,6 +149,7 @@
 					position: absolute;
 					left: 60rpx;
 					top: 0rpx;
+
 					image {
 						height: 100%;
 						width: 100%;
@@ -90,19 +161,21 @@
 				display: flex;
 				flex-direction: row;
 				place-items: center;
-				width: 784rpx;
+				width: 594rpx;
 				height: 72rpx;
 				background: #FFFFFF;
 				border-radius: 36rpx;
 				position: relative;
 				margin-left: 80rpx;
 
+				// margin-right: 10px;
 				.icon {
 					width: 20rpx;
 					height: 20rpx;
 					position: absolute;
-					left:20rpx;
-					top:15rpx;
+					left: 20rpx;
+					top: 15rpx;
+
 					image {
 						height: 100%;
 						width: 100%;
@@ -112,8 +185,9 @@
 				.input-box {
 					width: 100%;
 					position: absolute;
-					left:80rpx;
-					input{
+					left: 80rpx;
+
+					input {
 						font-size: 28rpx;
 						font-family: PingFangSC-Medium, PingFang SC;
 						font-weight: 500;
@@ -131,6 +205,7 @@
 					line-height: 40rpx;
 					position: absolute;
 					right: 7rpx;
+
 					button {
 						width: 118rpx;
 						height: 64rpx;
@@ -146,5 +221,127 @@
 				}
 			}
 		}
+
+		.scroll-list {
+			@include flex(column);
+			width: 95%;
+			height: 100%;
+			margin-left: 20rpx;
+			margin-top: 60rpx;
+			background-color: #FFFFFF;
+			border-radius: 16rpx;
+
+			&__goods-item {
+				margin-right: 20px;
+
+				&__image {
+					width: 60px;
+					height: 60px;
+					border-radius: 4px;
+				}
+
+				&__text {
+					color: #f56c6c;
+					text-align: center;
+					font-size: 12px;
+					margin-top: 5px;
+				}
+			}
+
+			&__show-more {
+				background-color: #fff0f0;
+				border-radius: 3px;
+				padding: 3px 6px;
+				@include flex(column);
+				align-items: center;
+
+				&__text {
+					font-size: 12px;
+					width: 12px;
+					color: #f56c6c;
+					line-height: 16px;
+				}
+			}
+		}
+
+		.shop-card-area {
+			height: auto;
+			background-color: null;
+			margin-left: 20rpx;
+			margin-right: 20rpx;
+			margin-top: 40rpx;
+
+			.shop-card {
+				width: 342rpx;
+				height: 558rpx;
+				background: #FFFFFF;
+				border-radius: 16rpx;
+
+
+				.image-box {
+					width: 342rpx;
+					height: 342rpx;
+
+					image {
+						width: 100%;
+						height: 100%;
+					}
+				}
+
+				.title {
+					width: 320rpx;
+					height: 80rpx;
+					font-size: 28rpx;
+					font-family: PingFangSC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #333333;
+					line-height: 40rpx;
+					margin-top: 20rpx;
+					margin-left: 20rpx;
+					margin-right: 20rpx;
+				}
+
+				.sec-title {
+					width: 258rpx;
+					height: 34rpx;
+					font-size: 24rpx;
+					font-family: PingFangSC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #999999;
+					line-height: 34rpx;
+					margin: 20rpx 0rpx 0rpx 20rpx;
+				}
+
+				.tag-location {
+					display: flex;
+					flex-direction: row;
+					margin-left: 20rpx;
+					margin-right: 20rpx;
+					margin-top: 20rpx;
+
+					.tag {
+						width: 44rpx;
+						height: 32rpx;
+						font-size: 22rpx;
+						font-family: PingFangSC-Regular, PingFang SC;
+						font-weight: 400;
+						color: #FF7A00;
+						line-height: 32rpx;
+					}
+
+					.location {
+						width: 88rpx;
+						height: 32rpx;
+						font-size: 22rpx;
+						font-family: PingFangSC-Regular, PingFang SC;
+						font-weight: 400;
+						color: #FF7A00;
+						line-height: 32rpx;
+					}
+				}
+
+			}
+		}
+
 	}
 </style>
