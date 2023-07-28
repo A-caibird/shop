01f7223909ad32f977764600949ff306dbb6cc75
Acaibird <lian.wk04201@foxmail.com>
2023-07-28 17:19:28 +0800

    feat,fix]:修复了unapp移动端页面滚动的弹性效果;美食列表完成,测试完成,文本过会隐>藏
            globalStyle: {
                    navigationBarTextStyle: black,
                    navigationBarTitleText: uni-app,
                    navigationBarBackgroundColor: #F8F8F8,
                    backgroundColor: #F8F8F8,
                    navigationStyle: custom,
                    app-plus: { //app平台
                            bounce: none //禁止滚动
                    }
    
            },

---
+++ components/food-list-item.vue
@@ -0,0 +1,257 @@
+<template>
+	<view>
+		<view class="list-area">
+			<view class="item" v-for="item in 4" :key="item">
+				<view class="left">
+					<view class="image-box">
+						<image src="@/static/foodList/foodItem1.png">
+						</image>
+					</view>
+				</view>
+				<view class="right">
+					<view class="title">
+						<text>
+							大叔小子烤鱼（天一广场店）
+						</text>
+					</view>
+					<view class="rate-price-sales">
+						<view class="rate">
+							<u-rate :count="5" v-model="rateValue" active-color="#69DB38"></u-rate>
+							<text>
+								{{rateValue}}
+							</text>
+						</view>
+						<view class="price">
+							<text>
+								¥60
+							</text>
+							<text>
+								/
+							</text>
+							<text>
+								人
+							</text>
+						</view>
+						<view class='sales'>
+							<text>
+								月售789
+							</text>
+						</view>
+					</view>
+					<view class="tag">
+						<view class="kinds">
+							<text>
+								烤鱼
+							</text>
+						</view>
+						<view class="location-text">
+							<text>
+								天一广场
+							</text>
+						</view>
+						<view class="distance">
+							<view class="icon">
+								<image src="@/static/location.png">
+
+								</image>
+							</view>
+							<view class="num">
+								<text>
+									120m
+								</text>
+							</view>
+						</view>
+					</view>
+					<view class="u-gap">
+
+					</view>
+					<view class="discount-list">
+						<view class="discount-list-item" v-for="item in 3 " :key="item">
+							<view class='tag'>
+								<text>
+									惠
+								</text>
+							</view>
+							<view class="price">
+								<text>¥555.9</text>
+							</view>
+							<view class="originPrice">
+								<text>¥435.9</text>
+							</view>
+							<view class="info">
+								<text>
+									2-3人超值烤鱼套餐
+								</text>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "food-list-item",
+		data() {
+			return {
+				rateValue: 0,
+			}
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.list-area {
+		@include __hw100();
+		@include __padding();
+		margin-top: 270rpx;
+
+		.item {
+			@include __padding();
+			@include flexX();
+			margin-top: 30rpx;
+			background: #FFFFFF;
+			border-radius: 16rpx;
+			width: 670rpx;
+			height: 348rpx;
+			position: relative;
+
+
+			.left {
+				.image-box {
+					height: 175rpx;
+					width: 160rpx;
+
+					image {
+						@include __hw100();
+					}
+
+					margin-right:20rpx;
+				}
+			}
+
+			.right {
+
+				.title {
+					width: 280px;
+					@include fontStyle(32rpx, 500, #333333, 44rpx);
+					@include textHidden();
+
+				}
+
+				.rate-price-sales {
+					@include flexX();
+					@include fontStyle(28rpx, 400, #666666, 34rpx);
+					gap: 20rpx;
+					margin-top: 20rpx;
+
+					.rate {
+						color: #48C368;
+						@include flexX();
+					}
+
+					.sales {
+						position: absolute; //相对于.right定位,最近的relative定位
+						right: 20rpx;
+						@include textHidden();
+
+						text {
+							@include fontStyle(24rpx, 400, #666666, 34rpx);
+						}
+					}
+				}
+
+				.tag {
+					margin: 20rpx 0;
+					@include flexX();
+					@include fontStyle(24rpx, 400, #666666, 34rpx);
+					flex-wrap: nowrap;
+					gap: 20rpx;
+
+					.distance {
+						position: absolute;
+						right: 20rpx;
+						@include flexX;
+						place-items: center;
+
+						.icon {
+							height: 28rpx;
+							width: 28rpx;
+
+							image {
+								@include __hw100();
+							}
+						}
+
+						.num {
+							@include textHidden();
+
+							text {
+								@include fontStyle(24rpx, 400, #666666, 34rpx);
+							}
+						}
+					}
+				}
+
+				.u-gap {
+					width: 100%;
+					height: 1rpx;
+					background: #EBEBEB;
+				}
+
+				.discount-list {
+					margin-top: 10rpx;
+
+					.discount-list-item {
+						@include flexX;
+						place-items: center;
+						gap: 10rpx;
+
+						.tag {
+							background: #FEF8E8;
+							border-radius: 8rpx;
+							@include textHidden();
+
+							text {
+								@include fontStyle(22rpx, 500, #FDAC03, 22rpx);
+							}
+						}
+
+						.price {
+							width: 100rpx;
+							@include textHidden();
+
+							text {
+								@include fontStyle(28rpx, 500, #FF7A00, 28rpx);
+							}
+						}
+
+						.originPrice {
+							@include textHidden();
+							width: 100rpx;
+							height:60rpx;
+							text-decoration: line-through;
+							text-decoration-color: #999999;
+							vertical-align: middle;
+							text {
+								@include fontStyle(22rpx, 500, #999999, 22rpx);
+							}
+						}
+
+						.info {
+							@include textHidden();
+							width: 260rpx;
+
+							text {
+								@include fontStyle(24srpx, 500, #333333, 24rpx);
+
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+</style>
--- pages/foodList/foodList.vue
+++ pages/foodList/foodList.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="container">
-		<u-navbar bgColor="#ffffff" leftIcon="" height="124">
+		<u-navbar bgColor="#ffffff" leftIcon="" height="124" :fixed="true">
 			<view class="u-nav-slot" slot="center">
 				<view class="nav-search">
 					<view class="box">
@@ -64,104 +64,14 @@
 				</view>
 			</view>
 		</u-navbar>
-		<view class="list-area" v-if="true">
-			<view class="item">
-				<view class='sales'>
-					<text>
-						月售789
-					</text>
-				</view>
-				<view class="distance">
-					<view class="icon">
-						<image src="../../static/location.png">
-
-						</image>
-					</view>
-					<view class="num">
-						<text>
-							120m
-						</text>
-					</view>
-				</view>
-				<view class="left">
-					<view class="image-box">
-						<image src="@/static/foodList/foodItem1.png">
-						</image>
-					</view>
-				</view>
-				<view class="right">
-					<view class="title">
-						<text>
-							大叔小子烤鱼（天一广场店）
-						</text>
-					</view>
-					<view class="rate-price-sales">
-						<view class="rate">
-							<u-rate :count="5" v-model="rateValue" active-color="#69DB38"></u-rate>
-							<text>
-								{{rateValue}}
-							</text>
-						</view>
-						<view class="price">
-							<text>
-								¥60
-							</text>
-							<text>
-								/
-							</text>
-							<text>
-								人
-							</text>
-						</view>
-					</view>
-					<view class="tag">
-						<view class="kinds">
-							<text>
-								烤鱼
-							</text>
-						</view>
-						<view class="location-text">
-							<text>
-								天一广场
-							</text>
-						</view>
-
-					</view>
-					<view class="u-gap">
-						<u-gap height="2" bgColor="#EBEBEB"></u-gap>
-					</view>
-					<view class="discount-list">
-						<view class="item" v-for="item in 1" :key="item">
-							<view class='tag'>
-								<text>
-									惠
-								</text>
-							</view>
-							<view class="price">
-								<text>¥15.9</text>
-							</view>
-							<view class="originPrice">
-								<text>¥75.9</text>
-							</view>
-							<view class="info">
-								<text>
-									2-3人超值烤鱼套餐
-								</text>
-							</view>
-						</view>
-					</view>
-				</view>
-			</view>
-		</view>
+		<foot-list-item></foot-list-item>
 	</view>
 </template>
 <script>
-	import footerBottom from "@/components/indexPage/footer.vue"
-	import functionOption from "@/components/indexPage/functionOption.vue"
+	import footListItem from "../../components/food-list-item.vue"
 	export default {
 		components: {
-			functionOption,
-			footerBottom,
+			footListItem
 		},
 		data() {
 			return {
@@ -180,62 +90,19 @@
 		mounted() {}
 	}
 </script>
-<style lang="scss">
-	@mixin __margin {
-		margin: 20rpx 20rpx;
-	}
-
-	@mixin __padding {
-		padding: 20rpx 20rpx;
-	}
-
-	@mixin flexY {
-		display: flex;
-		flex-direction: column;
-		// justify-content: space-between;
-		// justify-items: ;
-		// justify-self: ;
-		// place-item:center;
-	}
-
-	@mixin flexX {
-		display: flex;
-		flex-direction: row;
-		// justify-content: space-between;
-		// justify-items: ;
-		// justify-self: ; //子元素
-		// place-item:center;
-	}
-
-	@mixin fontStyle($size, $weight, $color, $height) {
-		font-size: $size;
-		font-family: PingFangSC-Regular, PingFang SC;
-		font-weight: $weight;
-		color: $color;
-		line-height: $height;
-	}
-
-	@mixin __hw100 {
-		height: 100%;
-		width: 100%;
-	}
-
-	@mixin textHidden {
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
-	page{
-		background: #F3F4F5;
-	}
+<style lang="scss" scoped>
 	.container {
 		@include __hw100();
+		background: #F3F4F5;
+
 		.u-nav-slot {
-			margin-top: 80rpx;
+			margin-top: 40rpx;
+
 			.nav-search {
 				@include flexX;
 				place-items: center;
 				@include __hw100();
+
 				.box {
 					width: 40rpx;
 					height: 40rpx;
@@ -312,9 +179,11 @@
 				gap: 90rpx;
 				margin-top: 30rpx;
 				margin-bottom: 30rpx;
+
 				.item {
 					position: relative;
 					@include flexX;
+
 					.box {
 						width: 20rpx;
 						height: 20rpx;
@@ -322,6 +191,7 @@
 						image {
 							@include __hw100();
 						}
+
 						position:relative;
 						left:3rpx;
 					}
@@ -329,6 +199,7 @@
 					.title {
 						overflow: hidden;
 						text-overflow: ellipsis;
+
 						text {
 							font-size: 28rpx;
 							font-family: PingFangSC-Regular, PingFang SC;
@@ -337,141 +208,6 @@
 							@include fontStyle(28rpx, 400, #333333, 28rpx);
 							text-overflow: ellipsis;
 							white-space: nowrap;
-						}
-					}
-				}
-			}
-		}
-
-		.list-area {
-			@include __hw100();
-			@include __padding();
-			height: 1000rpx;
-			margin-top: 270rpx;
-
-			.item {
-				@include __padding();
-				@include flexX();
-				margin-top: 20rpx;
-				background: #FFFFFF;
-				border-radius: 16rpx;
-				width: 670rpx;
-				height: 348rpx;
-				position: relative;
-
-				.sales {
-					position: absolute;
-					right: 20rpx;
-					top: 75rpx;
-					@include textHidden();
-					text {
-						@include fontStyle(24rpx, 400, #666666, 34rpx);
-					}
-				}
-
-				.distance {
-					position: absolute;
-					right: 20rpx;
-					top: 120rpx;
-					@include flexX;
-					place-items: center;
-
-					.icon {
-						height: 28rpx;
-						width: 28rpx;
-
-						image {
-							@include __hw100();
-						}
-					}
-
-					.num {
-						@include textHidden();
-						text {
-							@include fontStyle(24rpx, 400, #666666, 34rpx);
-						}
-					}
-				}
-
-				.left {
-					.image-box {
-						height: 175rpx;
-						width: 160rpx;
-
-						image {
-							@include __hw100();
-						}
-
-						margin-right:20rpx;
-					}
-				}
-
-				.right {
-
-					.title {
-						@include fontStyle(32rpx, 500, #333333, 44rpx);
-						@include textHidden();
-					}
-
-					.rate-price-sales {
-						@include flexX();
-						@include fontStyle(28rpx, 400, #666666, 34rpx);
-						gap: 20rpx;
-						margin-top: 20rpx;
-						position: relative;
-						right: 6rpx;
-
-						.rate {
-							color: #48C368;
-							@include flexX();
-						}
-					}
-
-					.tag {
-						margin: 20rpx 0;
-						@include flexX();
-						@include fontStyle(24rpx, 400, #666666, 34rpx);
-						flex-wrap: nowrap;
-						gap: 20rpx;
-					}
-
-					.discount-list {
-						.item {
-							@include flexX;
-							place-items: center;
-							position: relative;
-							right: 25rpx;
-							gap: 20rpx;
-
-							.tag {
-								background: #FEF8E8;
-								border-radius: 8rpx;
-
-								text {
-									@include fontStyle(22rpx, 500, #FDAC03, 32rpx);
-								}
-							}
-
-							.price {
-								text {
-									@include fontStyle(28rpx, 500, #FF7A00, 28rpx);
-								}
-							}
-
-							.originPrice {
-								text-decoration: line-through;
-								text-decoration-color: #999999;
-
-								text {
-									@include fontStyle(22rpx, 500, #999999, 22rpx);
-								}
-							}
-
-							.info {
-								text {
-									@include fontStyle(24rpx, 500, #333333, 32rpx);
-								}
-							}
 						}
 					}
 				}
