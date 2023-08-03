57378a0cafadb488dbca9f34d7b4a4596c193543
Acaibird <lian.wk04201@foxmail.com>
2023-08-03 17:06:39 +0800

    [feat]:两个优惠卷页面

---
+++ components/coupon-list/coupon-list-item1.vue
@@ -0,0 +1,138 @@
+<template>
+	<view>
+		<view class="item" >
+			<view class="tag">
+				<text>
+					通用
+				</text>
+			</view>
+			<view class="left">
+				<text>
+					平台通用优惠券
+				</text>
+				<text>
+					2023.02.15到期
+				</text>
+			</view>
+			<view class="right">
+				<view class="divi">
+
+				</view>
+				<view class="content">
+					<text>
+						¥10
+					</text>
+					<text>
+						满50可用
+					</text>
+					<view class="btn">
+						<text>
+							去使用
+						</text>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.item {
+
+		width: 100%;
+		overflow-y: auto;
+		position: relative;
+		@include flexX;
+		justify-content: space-between;
+		place-items: center;
+		box-sizing: border-box;
+		background: #ffffff;
+		border-radius: 16rpx;
+
+		.tag {
+			position: absolute;
+			left: 0;
+			top: 0;
+			background: #FEF8E8;
+			border-radius: 16rpx 0rpx 16rpx 0rpx;
+			display: inline;
+			padding: 5rpx;
+
+			text {
+				@include fontStyle(22rpx, 400, #FDAC03, 32rpx);
+			}
+		}
+
+		.left {
+			padding: 52rpx 64rpx;
+			@include flexY;
+			gap: 24rpx;
+
+			text {
+				&:first-child {
+					@include fontStyle(28rpx, 500, #333333, 40rpx);
+				}
+
+				&:last-child {
+					@include fontStyle(24rpx, 400, #999999, 34rpx);
+				}
+			}
+
+		}
+
+		.right {
+			margin: 28rpx 0;
+			margin-right: 32rpx;
+			@include flex;
+			gap: 20rpx;
+
+			&>.divi {
+				border-left: 1px dashed #999999;
+				margin: -28rpx 0;
+				margin-right: 32rpx
+			}
+
+			&>.content {
+				@include flexY;
+				place-items: center;
+				gap: 10rpx;
+
+				text {
+					display: block;
+
+					&:nth-child(1) {
+						@include fontStyle(52rpx, 500, #48C368, 74rpx);
+					}
+
+					&:nth-child(2) {
+						@include fontStyle(24rpx, 400, #999999, 34rpx);
+					}
+				}
+
+				.btn {
+					background: linear-gradient(132deg, #69DB38 0%, #48C368 100%);
+					border-radius: 36rpx;
+					padding: 8rpx 24rpx;
+
+					text {
+						@include fontStyle(24rpx, 500, #ffffff, 34rpx);
+					}
+				}
+			}
+
+		}
+	}
+</style>
---
+++ pages/coupon/coupon.vue
@@ -0,0 +1,184 @@
+<template>
+	<view class="container">
+		<u-navbar height="98" leftIcon="">
+			<view class="u-nav-slot" slot="center">
+				<view class="back">
+					<image src="@/static/back.png">
+					</image>
+				</view>
+				<view class="title">
+					<text>
+						优惠券
+					</text>
+				</view>
+				<view class="selection">
+					<view class="selectNav" :class="{active:selected[0]}" @tap="select(0)">
+						<text>
+							平台优惠券
+						</text>
+					</view>
+					<view class="selectNav" :class="{active:selected[1]}" @tap="select(1)">
+						<text>
+							店铺优惠券
+						</text>
+					</view>
+				</view>
+			</view>
+		</u-navbar>
+		<template v-if="selected[0]">
+			<view class="list1">
+				<list-item1 v-for="item of 10" :key="item"></list-item1>
+			</view>
+		</template>
+		<template v-else>
+			<view class="list2">
+				<view class="item" v-for="item of 4" :key="item">
+					<view class="store">
+						<view class="image-box">
+							<image src="@/static/avatar.png">
+							</image>
+						</view>
+
+						<text>
+							赛吉-泰式按摩（天一广场店）
+						</text>
+					</view>
+					<list-item1 v-for="item of 2" :key="item"></list-item1>
+				</view>
+
+			</view>
+		</template>
+	</view>
+</template>
+
+<script>
+	import listItem1 from "@/components/coupon-list/coupon-list-item1.vue"
+	export default {
+		components: {
+			listItem1
+		},
+		data() {
+			return {
+				selected: [true, false]
+			}
+		},
+		methods: {
+			select(num) {
+				this.selected = this.selected.map(function(item, index) {
+					return index == num;
+				});
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.container {
+		@include full-screen-color;
+		background: #f3f4f5;
+		overflow-y: auto;
+		padding: 24rpx 24rpx;
+
+		.u-nav-slot {
+			width: 100%;
+			padding: 22rpx 24rpx;
+			@include flexY;
+			position: relative;
+
+			.back {
+				position: absolute;
+				left: 22rpx;
+				top: 24rpx;
+
+				image {
+					width: 40rpx;
+					height: 40rpx;
+				}
+			}
+
+			.title {
+				align-self: center;
+				margin-bottom: 40rpx;
+
+				text {
+					@include fontStyle(32rpx, 500, #333333, 44rpx);
+				}
+			}
+
+			.selection {
+				width: 100%;
+				padding: 0 94rpx;
+				box-sizing: border-box;
+				@include flexX;
+				justify-content: space-between;
+
+				.selectNav {
+					position: relative;
+
+					text {
+						@include fontStyle(28rpx, 400, #333333, 40rpx);
+					}
+
+					&.active {
+						text {
+							color: #48C368;
+						}
+
+						&::after {
+							content: "";
+							position: absolute;
+							width: 40rpx;
+							height: 4rpx;
+							position: absolute;
+							top: 50rpx;
+							left: 50%;
+							transform: translateX(-50%);
+							background: #48C368;
+							transform: translateZ(0);
+						}
+					}
+				}
+			}
+		}
+
+
+		.list1 {
+			width: 100%;
+			position: relative;
+			top: 230rpx;
+			@include flexY;
+			gap: 20rpx 0;
+			padding-bottom: 30rpx;
+		}
+
+		.list2 {
+			width: 100%;
+			position: relative;
+			top: 230rpx;
+			@include flexY;
+			gap: 30rpx 0;
+			padding-bottom: 30rpx;
+			.item{
+				@include flexY;
+				gap:20rpx 0;
+				.store {
+					@include flexX;
+					gap:0 10rpx;
+					place-items: center;
+					.image-box {
+						border-radius: 50%;
+						width: 40rpx;
+						height: 40rpx;
+						image {
+							@include __hw100;
+						}
+					}
+					text {
+						display: block;
+						@include fontStyle(34rpx, 400, #5C5C5C, 34rpx);
+					}
+				}
+			}
+		}
+	}
+</style>
